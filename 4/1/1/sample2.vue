8491d15efc5db24be45f62afc9e19ea06133410f
Lifei Li <lupuswere@gmail.com>
2020-11-24 16:08:44 -0800

    Sample 2 4.1.1

---
+++ 4/1/1/sample2.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>{{ title }}</h2>
+        <p>{{ profile }}</p>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Instance',
+        data () {
+            return {
+                title: 'A Vue App'
+            }
+        },
+        created () {
+            Object.assign(this.$data, {
+                profile: 'This is a Vue App'
+            });
+            console.log(this.$data);
+        }
+    }
+</script>
